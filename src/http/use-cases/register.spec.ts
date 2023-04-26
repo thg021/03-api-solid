9b1feeaff273e6bf5e43114ec098e2e741178dc1
thiago da silva <thg021@gmail.com>
2023-04-25 23:29:36 -0300

    feat: Design Patterns & Testes - Primeiro teste unitário

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('RegisterUsecase unit test', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+    const password = '123456'
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password,
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      password,
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBeTruthy()
+  })
 })
