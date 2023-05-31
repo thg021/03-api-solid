ddda45887206e0666d52b5b03373e7c8e6ed4318
thiago da silva <thg021@gmail.com>
2023-05-31 07:38:40 -0300

    feat: implementacao do metodo findById do repositorio prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,8 +3,14 @@ import { Prisma, User } from '@prisma/client'
 import { IUsersRepository } from '../users-repository.interface'
 
 export class PrismaUsersRepository implements IUsersRepository {
-  findById(userId: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(userId: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+
+    return user
   }
 
   async findByEmail(email: string): Promise<User | null> {
