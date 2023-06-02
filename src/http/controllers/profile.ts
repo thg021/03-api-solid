3abf4ec1c581314a8fb9e03f2006ba6d3a628704
thiago da silva <thg021@gmail.com>
2023-06-02 16:29:53 -0300

    feat: integrando o controller com useCase

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,15 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
-  console.log('Token', request.user.sub)
-  return reply.status(200).send()
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
