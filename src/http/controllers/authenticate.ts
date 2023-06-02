8ceecfeb9b38af4a233f824c672fd1abbb20b1c3
thiago da silva <thg021@gmail.com>
2023-06-02 16:05:42 -0300

    feat: adicionado jwt nas chamda authenticate e profile

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,5 +1,3 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
@@ -18,7 +16,20 @@ export async function authenticate(
 
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (e) {
     if (e instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: e.message })
@@ -26,6 +37,4 @@ export async function authenticate(
 
     throw e
   }
-
-  return reply.status(200).send()
 }
