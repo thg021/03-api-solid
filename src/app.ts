3fc2a480d4e646990cf38e22ac5f67d1873d187e
thiago da silva <thg021@gmail.com>
2023-06-07 07:31:50 -0300

    feat: adicionado rota de checkins no app

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import fastifyJwt from '@fastify/jwt'
 
 import { usersRoutes } from '@/http/controllers/users/routes'
 import { gymsRoutes } from '@/http/controllers/gyms/routes'
+import { checkInsRoutes } from '@/http/controllers/check-ins/routes'
 export const app = fastify()
 
 app.register(fastifyJwt, {
@@ -14,6 +15,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
