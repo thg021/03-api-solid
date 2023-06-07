14aed0ac238fecc877c494acda3b4340d7de556e
thiago da silva <thg021@gmail.com>
2023-06-07 07:31:20 -0300

    refactor: retirado importações que nao estavam sendo utilizadas

--- src/http/controllers/users/register.ts
+++ src/http/controllers/users/register.ts
@@ -1,9 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
-import { RegisterUseCase } from '../../use-cases/register'
-import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
-import { UserAlreadyExistsError } from '../../use-cases/errors/user-already-exists-error'
+
 import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
